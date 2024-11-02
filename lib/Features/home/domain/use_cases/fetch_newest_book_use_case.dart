62da55a3b3096a18dfbe8af6310ca3b12868f91e
carnage67694325 <kingabdoeid2014@gmail.com>
2024-11-02 19:04:43 +0200

    created fetch newest book use case

---
+++ lib/Features/home/domain/use_cases/fetch_newest_book_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:clean_arc_bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:clean_arc_bookly/Features/home/domain/repos/home_repo.dart';
+import 'package:clean_arc_bookly/core/errors/failure.dart';
+import 'package:clean_arc_bookly/core/use_case/use_case.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchBooksNewsetUseCase implements UseCase<List<BookEntity>, void> {
+  final HomeRepo homeRepo;
+
+  FetchBooksNewsetUseCase({required this.homeRepo});
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void param]) async {
+    return await homeRepo.fetchNewestBooks();
+  }
+}
