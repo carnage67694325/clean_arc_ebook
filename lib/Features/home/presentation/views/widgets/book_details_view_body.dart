04abe3f026b50fd4df611192086e899ea2c78d84
carnage67694325 <kingabdoeid2014@gmail.com>
2024-11-02 14:25:27 +0200

    added ui

---
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -0,0 +1,43 @@
+import 'package:clean_arc_bookly/Features/home/presentation/views/widgets/similar_books_section.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'books_action.dart';
+import 'books_details_sectioni.dart';
+import 'custom_book_details_app_bar.dart';
+import 'custom_book_item.dart';
+
+class BookDetailsViewBody extends StatelessWidget {
+  const BookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                CustomBookDetailsAppBar(),
+                BookDetailsSection(),
+                Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                SimilarBooksSection(),
+                SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
