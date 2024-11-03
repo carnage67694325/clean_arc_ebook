8472ca3e6040586bb15f789a8abab4c772e68340
carnage67694325 <kingabdoeid2014@gmail.com>
2024-11-03 15:01:10 +0200

    register book entity adapter and opend featured box

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:clean_arc_bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:clean_arc_bookly/constants.dart';
 import 'package:clean_arc_bookly/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
 import 'Features/Splash/presentation/views/splash_view.dart';
 
-void main() {
+void main() async {
   runApp(const Bookly());
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class Bookly extends StatelessWidget {
